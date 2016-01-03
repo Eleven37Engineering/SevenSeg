98cf1a5185c4f0561aeb95301b123ef0321f31ea
Damon Earl <damon.earl@gmail.com>
2016-01-02 18:48:31 -0700

    Created basic header and source file.
    Create the basic outline of source file and updated header file to
    include return value.

---
+++ SevenSeg.cpp
@@ -0,0 +1,21 @@
+#include <SevenSeg.hpp>
+
+SevenSeg::SevenSeg()
+{
+	setup();
+}
+
+void SevenSeg::SetDisplay(uint8_t digit1, uint8_t digit2, uint8_t digit3, uint8_t digit4)
+{
+	
+}
+
+void SevenSeg::setup()
+{
+
+}
+
+void SevenSeg::setSegments(int number, int digit, int time)
+{
+	
+}
--- SevenSeg.hpp
+++ SevenSeg.hpp
@@ -5,10 +5,10 @@ class SevenSeg
 {
 	public:
 		SevenSeg();
-		SetDisplay(uint8_t digit1, uint8_t digit2, uint8_t digit3, uint8_t digit4);
+		void SetDisplay(uint8_t digit1, uint8_t digit2, uint8_t digit3, uint8_t digit4);
 
 
 	private:
-		setup();
-		setSegments(int number, int digit, int time);
+		void setup();
+		void setSegments(int number, int digit, int time);
 }
