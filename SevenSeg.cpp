31224ed798ee2a9279f06dadbe840602a816dd5f
Damon Earl <damon.earl@gmail.com>
2016-01-03 03:07:15 -0700

    More ascii testing
    Still shoving to uppercase.

--- SevenSeg.cpp
+++ SevenSeg.cpp
@@ -125,13 +125,10 @@ void SevenSeg::SetDisplay(uint8_t digit1, uint8_t digit2, uint8_t digit3, uint8_
 
 void SevenSeg::SetDisplay(char* value, uint32_t time)
 {
-  String string = value;
-  string.toUpperCase();
-  value = string;
-  uint32_t digit1 = value[0] - 48;
-  uint32_t digit2 = value[1] - 48;
-  uint32_t digit3 = value[2] - 48;
-  uint32_t digit4 = value[3] - 48;
+  uint32_t digit1 = (value[0] & 0x20) - 48;
+  uint32_t digit2 = (value[1] & 0x20) - 48;
+  uint32_t digit3 = (value[2] & 0x20) - 48;
+  uint32_t digit4 = (value[3] & 0x20) - 48;
 
   if(digit1 > 10)
   {
