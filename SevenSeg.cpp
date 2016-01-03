35e2c64d9036944f610724b150baaf6bc3c11141
Brody Ricketts <brody.ricketts@gmail.com>
2016-01-02 19:38:53 -0700

    change to SevenSeg.cpp
    Added numbers array. Adjusted formatting of code.

--- SevenSeg.cpp
+++ SevenSeg.cpp
@@ -10,40 +10,54 @@ void SevenSeg::SetDisplay(uint8_t digit1, uint8_t digit2, uint8_t digit3, uint8_
 	
 }
 
+const byte numbers[10][8] = 
+{ 
+  {1, 1, 1, 1, 1, 1, 0, 0}, // 0
+  {0, 1, 1, 0, 0, 0, 0, 0}, // 1
+  {1, 1, 0, 1, 1, 0, 1, 0}, // 2
+  {1, 1, 1, 1, 0, 0, 1, 0}, // 3
+  {0, 1, 1, 0, 0, 1, 1, 0}, // 4
+  {1, 0, 1, 1, 0, 1, 1, 0}, // 5
+  {1, 0, 1, 1, 1, 1, 1, 0}, // 6
+  {1, 1, 1, 0, 0, 0, 0, 0}, // 7
+  {1, 1, 1, 1, 1, 1, 1, 0}, // 8
+  {1, 1, 1, 0, 0, 1, 1, 0}  // 9 
+};
+
 void SevenSeg::setup()
 {
 	pinMode(A, OUTPUT);
-    digitalWrite(A, LOW);
-  	pinMode(B, OUTPUT);
-  	digitalWrite(B, LOW);
-  	pinMode(C, OUTPUT);
-  	digitalWrite(C, LOW);
-  	pinMode(D, OUTPUT);
-  	digitalWrite(D, LOW);
-  	pinMode(E, OUTPUT);
-  	digitalWrite(E, LOW);
-  	pinMode(F, OUTPUT);
-  	digitalWrite(F, LOW);
-  	pinMode(G, OUTPUT);
-  	digitalWrite(G, LOW);
-  	pinMode(DP, OUTPUT);
-  	digitalWrite(DP, LOW);
+  digitalWrite(A, LOW);
+  pinMode(B, OUTPUT);
+  digitalWrite(B, LOW);
+  pinMode(C, OUTPUT);
+  digitalWrite(C, LOW);
+  pinMode(D, OUTPUT);
+  digitalWrite(D, LOW);
+  pinMode(E, OUTPUT);
+  digitalWrite(E, LOW);
+  pinMode(F, OUTPUT);
+  digitalWrite(F, LOW);
+  pinMode(G, OUTPUT);
+  digitalWrite(G, LOW);
+  pinMode(DP, OUTPUT);
+  digitalWrite(DP, LOW);
 
-  	// Set Common Cathodes to High to ensure all digits are off. 
-  	pinMode(CC1, OUTPUT);
-  	digitalWrite(CC1, HIGH);
-  	pinMode(CC2, OUTPUT);
-  	digitalWrite(CC2, HIGH);
-  	pinMode(CC3, OUTPUT);
-  	digitalWrite(CC3, HIGH);
-  	pinMode(CC4, OUTPUT);
-  	digitalWrite(CC4, HIGH);
+  // Set Common Cathodes to High to ensure all digits are off. 
+  pinMode(CC1, OUTPUT);
+  digitalWrite(CC1, HIGH);
+  pinMode(CC2, OUTPUT);
+  digitalWrite(CC2, HIGH);
+  pinMode(CC3, OUTPUT);
+  digitalWrite(CC3, HIGH);
+  pinMode(CC4, OUTPUT);
+  digitalWrite(CC4, HIGH);
 }
 
 void SevenSeg::setSegments(int number, int digit, int time)
 {
 	for(int seg = 0; seg < 8; seg++)
-  	{
+  {
     // Puts 5V to the anode of the segment if the numbers array shows a 1
     if(numbers[number][seg] == 1)
     {
@@ -54,4 +68,8 @@ void SevenSeg::setSegments(int number, int digit, int time)
     {
 		digitalWrite(segments[seg], LOW);
     }
+  }
 }
+
+
+
